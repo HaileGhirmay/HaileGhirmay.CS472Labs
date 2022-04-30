a31aece8d9e5d0d2294315f39df6610740e861b2
Haile@miu <hgpencilpower@gmail.com>
2022-04-30 07:24:25 -0500

    php refacored

--- w1d4_grades/params.php
+++ w1d4_grades/params.php
@@ -5,7 +5,7 @@ $id = $_POST['id'];
 
 if (strlen($name) == 0 || strlen($id) == 0) {
     echo "<script>alert('Please fill form fields');</script>";
-} elseif ($name == 'Mesut Kutlu' && $id == '613782') {
+} elseif ($name == 'Haile Ghirmay Tesfay' && $id == '614139') {
     echo "Your grade is 93.67";
 } else {
     echo "<script>alert('Grade not found.');</script>";
